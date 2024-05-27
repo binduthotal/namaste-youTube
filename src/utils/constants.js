c045d2482c0872456a230b2e130ff1601f2f8704
binduthotal <bindus170@gmail.com>
2024-05-27 11:21:10 +0530

    key is secured

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -89,7 +89,7 @@ export const commentsData = [
 
 export const OFFSET_LIVE_CHAT_COUNT = 10;
 
-const GOOGLE_API_KEY = "AIzaSyApNATN77n93SeEl45cxrx0l2APVvtAI8k";
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
     "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
